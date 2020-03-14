c69170646327d79228ddc6c8e4685dbf5228f815
Alex <alexklchoy.code@gmail.com>
2020-03-15 00:41:31 +0800

    updated insertion sort logic

--- insertion-sort.js
+++ insertion-sort.js
@@ -6,13 +6,20 @@ main();
 
 function insertionSort(array) {
     for(var _currentPosition=1; array.length>_currentPosition; _currentPosition++) {
-        var _swapKey = _currentPosition;
-        for(_swapKey; _swapKey>0; _swapKey--) {
+        let _key = _currentPosition;
+	   console.log("current key: " + _key);
+        for(let _swapKey=_key; _swapKey>=0; _swapKey--) {
+console.log("swap key: " + _swapKey);
             if(array[_swapKey-1] > array[_swapKey]) {
-                var tempSort = array[_swapKey]:
+                let tempSort = array[_swapKey];
                 array[_swapKey] = array[_swapKey - 1];
-            }
-            break;
+		array[_swapKey - 1] = tempSort;
+            console.log(array);
+
+	    } else {
+		break;
+	    }
         }
     }
+	console.log(array);
 }
